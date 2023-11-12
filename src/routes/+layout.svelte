063640283661ea2eea2f6ab9f04fad7acceb5db9
philipp-schuetz <mail@philippschuetz.com>
2023-11-12 17:58:29 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '@picocss/pico';
+</script>
+
+<slot />
---
+++ src/routes/[word]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { BookIcon } from 'svelte-feather-icons';
+</script>
+
+<svelte:head>
+	<title>Zuhause</title>
+	<meta name="description" content="Zangendeutsch Wörterbuch" />
+</svelte:head>
+<div />
+<BookIcon />
+<input type="search" id="search" name="search" placeholder="Search" />
