4bc3834bab9dd2f36f520f5ec34ce33569b1f3dc
Philipp Schütz <mail@philippschuetz.com>
2023-11-25 15:30:04 +0000

    Make page react to route param changes

--- src/routes/[[word]]/+page.svelte
+++ src/routes/[[word]]/+page.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import { BookIcon } from 'svelte-feather-icons';
-	export let data;
+	import { page } from '$app/stores';
+	import { getPossibleWords, getTranslations, getOriginals } from '$lib/dataAccess';
+	let search: string = '';
+	let previousParam = '';
+	if ($page.params.word === undefined) {
+		search = '';
+	} else {
+		search = $page.params.word;
+	}
+
+	$: {
+		if ($page.params.word !== undefined && previousParam !== $page.params.word) {
+			previousParam = $page.params.word;
+			search = $page.params.word;
+		}
+	}
+
+	let suggestions: string[];
+	$: suggestions = getPossibleWords(search);
 </script>
 
 <svelte:head>
@@ -9,5 +27,13 @@
 </svelte:head>
 <div />
 <BookIcon />
-<span>{data.word.original}</span>
-<input type="search" id="search" name="search" placeholder="Search" />
+<input type="search" id="search" name="search" placeholder="Search" bind:value={search} />
+Word: {search}
+<br />
+<h4>Search Suggestions</h4>
+{#each suggestions as suggestion}
+	<a href="/{suggestion}">{suggestion}</a><br />
+{/each}
+
+Test:
+{$page.params.word}
