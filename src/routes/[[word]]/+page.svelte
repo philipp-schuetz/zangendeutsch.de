5e9f7c5076a228f8ae7711b38ef7a931734d8dce
philipp-schuetz <mail@philippschuetz.com>
2024-11-06 17:02:07 +0100

    Update github username

--- src/routes/[[word]]/+page.svelte
+++ src/routes/[[word]]/+page.svelte
@@ -86,7 +86,7 @@
 		>
 		<a
 			class="secondary"
-			href="https://github.com/philipp-schuetz/zangendeutsch.de">Mitmachen</a
+			href="https://github.com/philippschuetz/zangendeutsch.de">Mitmachen</a
 		>
 	</footer>
 </article>
